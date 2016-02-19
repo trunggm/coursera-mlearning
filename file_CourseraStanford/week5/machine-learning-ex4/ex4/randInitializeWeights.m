aefa6c626f958f3ba4a0c3a315897765c9bd2a43
trunggm <trung.gm.vn@gmail.com>
2016-02-19 11:16:35 +0700

    week5 100/100 ok

--- file_CourseraStanford/week5/machine-learning-ex4/ex4/randInitializeWeights.m
+++ file_CourseraStanford/week5/machine-learning-ex4/ex4/randInitializeWeights.m
@@ -1,4 +1,4 @@
-xfunction W = randInitializeWeights(L_in, L_out)
+function W = randInitializeWeights(L_in, L_out)
 %RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
 %incoming connections and L_out outgoing connections
 %   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
@@ -19,7 +19,7 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first row of W corresponds to the parameters for the bias units
 %
 % khoi tao gia tri epsilon
-epsilon = 0.1;
+epsilon = 0.12;
 % Random gia tri trong so W
 W = rand(L_out, 1+L_in)*2*epsilon - epsilon;
 
