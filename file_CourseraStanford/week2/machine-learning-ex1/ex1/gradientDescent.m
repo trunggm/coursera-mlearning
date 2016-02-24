ab8c585ef7a2f3b784b2e071fc66dbc64b37448d
trunggm <trung.gm.vn@gmail.com>
2016-02-24 18:48:54 +0700

    week10 + week11

--- file_CourseraStanford/week2/machine-learning-ex1/ex1/gradientDescent.m
+++ file_CourseraStanford/week2/machine-learning-ex1/ex1/gradientDescent.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+X_temp = [ones(size(X)) X];
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,17 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+      % tinh gia tri ham gia thuyet h
+      h = X_temp*theta';
+      % tinh sai so
+      error = h - y;
+      % tinh gia tri theta
+      theta = theta - alpha/m*(error'*X_temp);
 
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+    J_history(iter) = 1/2/m*sum(error.^2);
 
 end
 
